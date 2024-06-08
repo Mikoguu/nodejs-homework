7bddd04656a19c4eae0753a68ea353d08356cd8c
Ekaterina Paramonova <lol.nanny@mail.ru>
2024-06-08 12:59:15 +0300

    implementes async version of image sorting

---
+++ js/sortImages1.js
@@ -0,0 +1,54 @@
+import { read } from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+
+const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.split('');
+
+const mainDirectory = process.cwd();
+const unsortedImagesDirectory = 'unsortedImages';
+const targetDirectory = 'images';
+
+let images  = [];
+
+async function throughDirectory(dir) {
+    
+    const readableDir = await fs.readdir(dir);
+    readableDir.shift();
+
+    for (let file of readableDir) {
+        const absolute = path.join(dir, file);
+        const stat = await fs.stat(absolute);
+        if (!stat.isDirectory()) {
+            images.push(absolute);
+        } else {  
+            await throughDirectory(absolute);
+        }
+    }
+};
+
+try {
+
+    if (!fs.stat(targetDirectory)) {
+       await fs.mkdir(`./${targetDirectory}`);
+    };
+
+    for (let i = 0; i < alphabet.length; i++) {
+        if (!fs.stat(`${targetDirectory}/${alphabet[i]}`)) {
+            await fs.mkdir(`${targetDirectory}/${alphabet[i]}`);
+        };
+    }; 
+
+await throughDirectory(`${mainDirectory}/${unsortedImagesDirectory}`);
+
+images.forEach(item => {
+    let itemData = path.parse(item);
+    for (let i = 0; i < alphabet.length; i++) {
+        if (alphabet[i] === itemData.name[0]) {
+            fs.rename( item, `${mainDirectory}/${targetDirectory}/${alphabet[i]}/${itemData.base}`);
+        };
+    };
+}); 
+
+} catch (err) {
+    throw err;
+};
